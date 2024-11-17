fcc732f8df30b30d552a5b5343e79c6442d89b9c
juicington <justintsourounis@gmail.com>
2024-11-17 18:34:00 -0500

    Update sketch.js
    paddle, ball, and bricks exist as objects, more or less in the their right place. Ball and Bricks still have no functionality.

--- sketch.js
+++ sketch.js
@@ -1,8 +1,27 @@
 var paddle;
+var ball;
+var bricks = [];
+var brickCol = 5;
+var brickRow = 5;
+var brickW = 100;
+var brickH = 20;
+var xOffset = brickW + 20;
+var yOffset = brickH + 20;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   paddle = new Paddle();
+  ball = new Ball();
+  brick = new Brick();
+
+  for (i = 0; i < brickRow; i++) {
+    for (j = 0; j < brickCol; j++) {
+      let x = 50 + j * xOffset;
+      let y = 50 + i * yOffset;
+      brick = new Brick(x, y);
+      bricks.push(brick);
+    }
+  }
 }
 
 function draw() {
@@ -10,4 +29,9 @@ function draw() {
 
   paddle.display();
   paddle.move(15);
+  ball.show();
+  
+  for (var brick of bricks) {
+  brick.show();
+  }
 }
