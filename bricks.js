8e9854a2dd002d971e7a4093bb57d77a7a404d36
juicington <justintsourounis@gmail.com>
2024-11-17 18:32:25 -0500

    Update bricks.js

--- bricks.js
+++ bricks.js
@@ -1,6 +1,12 @@
-class Bricks {
-  constructor(x, y) {
+class Brick {
+  constructor(x, y, w, h) {
     this.x = x;
     this.y = y;
+    this.w = 100;
+    this.h = 20;
+  }
+  
+  show() {
+    rect(this.x, this.y, this.w, this.h);
   }
 }
