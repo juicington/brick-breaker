f9645941dd0882e649dd30a63918d5e3ea7ab5f1
juicington <justintsourounis@gmail.com>
2025-01-29 16:54:45 -0500

    Update bricks.js

--- bricks.js
+++ bricks.js
@@ -1,12 +1,16 @@
-class Brick {
-  constructor(x, y, w, h) {
-    this.x = x;
-    this.y = y;
-    this.w = 100;
-    this.h = 20;
-  }
+class Block {
+    constructor(x, y) {
+      this.width = canvasWidth / 7; // Adjust relative to canvas width
+      this.height = canvasHeight / 30; // Adjust relative to canvas height
+      this.x = x;
+      this.y = y;
+    }
   
-  show() {
-    rect(this.x, this.y, this.w, this.h);
+    show() {
+   
+    strokeWeight(2);
+    fill(155, 20, 150);
+    rectMode(CENTER);
+    rect(this.x, this.y, this.width, this.height);
+    }
   }
-}
